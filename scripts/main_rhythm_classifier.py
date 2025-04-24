d2a68e61aa1b92fe5dab43efefd8855c27f95de4
Kiyoto ICHIKAWA <ichikawakiyoto@gmail.com>
2025-04-24 08:05:36 +0100

    add comments and docs

--- scripts/main_rhythm_classifier.py
+++ scripts/main_rhythm_classifier.py
@@ -13,45 +13,83 @@ from utils.data_loader import expected_results_df
 
 
 def _classifiation_by_computed_metrics(clf):
+    '''
+    Classifitacion using computed metrics
+
+    @Args
+    clf: Rhythm classifier object
+    @Returns
+    res_df (pd.DataFrame): result in DataFramw
+    '''
     all_res = {}
+    # --- each data file
     for df, file_name in iter_data_df():
         res_dic = {}
+        # --- each channel
         for ch in clf.data_processor.channels:
+            # --- classification
             ret = clf.is_irregular(df, ch)
-            res = 'AFib' if ret else 'Sinus'
+            # --- label to the classificatoin result
+            res = clf.label(ret)
+            # --- store result
             res_dic[ch] = res
+        # --- all channel
+        # --- classification
         ret = clf.is_irregular(df)
+        # --- label to the classificatoin result
         res = clf.label(ret)
+        # --- store result
         res_dic['All'] = res
+        # --- store all result for each file
         all_res[file_name] = res_dic
+    # --- dictionary to pd.DataFrame
     res_df = pd.DataFrame(all_res).T
     return res_df
 
 
 def _classifiation_by_expected_metrics(clf):
+    '''
+    Classifitacion using expected metrics
+
+    @Args
+    clf: Rhythm classifier object
+    @Returns
+    res_df (pd.DataFrame): result in DataFramw
+    '''
     df = expected_results_df()
     df.index = df.File
     all_res = {}
+    # --- for each row
     for i, row in df.iterrows():
+        # --- classification
         ret = clf._predict({'expected': dict(row)})
+        # --- label to the classificatoin result
         res = clf.label(ret)
+        # --- store result
         all_res[i] = {'Expetcted': res}
+    # --- dictionary to pd.DataFrame
     res_df = pd.DataFrame(all_res).T
     return res_df
 
 
 def main():
-    clf = RhythmClassifier()
-    computed_res_df = _classifiation_by_computed_metrics(clf)
+    # --- directory to store results
     res_dir = os.path.join(ROOT_DIR, 'res', 'classification')
     if not os.path.exists(res_dir):
         os.makedirs(res_dir)
+    # --- classifier object
+    clf = RhythmClassifier()
+    # --- classifitacion using computed metrics
+    computed_res_df = _classifiation_by_computed_metrics(clf)
+    # --- cassifitacion using expected metrics
+    expected_res_df = _classifiation_by_expected_metrics(clf)
+    # --- store classifiation result in csv
     file_path = os.path.join(res_dir, 'sinus_afib.csv')
     computed_res_df.to_csv(file_path)
     print('Saved: %s' % file_path)
+    # --- print result
     print('Classification results from computed metrics')
     print(computed_res_df)
-    expected_res_df = _classifiation_by_expected_metrics(clf)
     print('Classification results from expected metrics')
     print(expected_res_df)
 
